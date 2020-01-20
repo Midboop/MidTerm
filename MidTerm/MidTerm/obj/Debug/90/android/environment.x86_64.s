c4bf4b602c5e6153910550fe2481df60d7216479
dtomei13 <57144894+dtomei13@users.noreply.github.com>
2020-01-20 16:38:29 -0700

    Changes made on closing
    Changes made when closing VS?

--- MidTerm/MidTerm/obj/Debug/90/android/environment.x86_64.s
+++ MidTerm/MidTerm/obj/Debug/90/android/environment.x86_64.s
@@ -62,7 +62,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"df1055e1-809b-4df8-a088-fdcda8ccc18a"
+	.asciz	"0dc3e492-1c1e-4bdc-9628-de539ed15b9a"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
